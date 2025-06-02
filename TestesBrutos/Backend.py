956200f011c012f13d30e5eecf3168207c78a5a5
Dev Nadson <nadson.pros@gmail.com>
2025-06-02 17:11:48 -0300

    Fix: Campos atualizados

--- TestesBrutos/Backend.py
+++ TestesBrutos/Backend.py
@@ -1,31 +1,11 @@
-# Criar um aplicativo desktop para uma psicóloga
-
-# 1. Cadastro de Pacientes
-# Campos: Nome completo/ Data de nascimento/ Gênero/ CPF / RG/ Endereço/ Contato/ Nome do responsável (se menor de idade)/ Plano de saúde (se aplicável)
-# Observações adicionais: Data do primeiro atendimento
-
-# 2. Agenda / Calendário
-# Visualização diária, semanal e mensal
-# Marcar sessões com: Data e hora/ Duração/ Tipo (presencial, online)/ Status (agendada, realizada, cancelada, falta)/ Alertas / notificações/ Reagendamento com facilidade
-
-# 3. Prontuário do Paciente
-# Histórico de sessões/ Evolução do paciente (campo para texto livre)/ Diagnósticos (se aplicável)/ Estratégias de tratamento/ Anexos (áudios, PDFs, imagens — ex: laudos)/ Campo para resumo da sessão
-
-# 4. Financeiro
-# Controle por paciente e por data
-# Campos: Valor da consulta/ Forma de pagamento (PIX, dinheiro, cartão, plano)/ Status (pago, pendente)/ Datas de pagamento/ Geração de recibo (PDF)/ Relatórios mensais/anuais
-
-# 5. Relatórios
-# Filtros por: Período/ Paciente/ Tipo de atendimento/ Status de pagamento/ Exportação: PDF / Excel
-
-# 6. Login Seguro
-# Login com senha/ Backup local ou na nuvem (criptografado)/ Criptografia de dados sensíveis
 import os
 
 class Paciente:
-    def __init__(self, name, age): # birthday, gender, CPF, RG, adress, contact, id):
+    def __init__(self, name, age, civil_status, profession): # birthday, gender, CPF, RG, adress, contact, id):
         self.name = name
         self.age = age
+        self.civil_status = civil_status
+        self.profession = profession
         # self.birthday = birthday
         # self.gender = gender
         # self.CPF = CPF
@@ -39,28 +19,39 @@ class Paciente:
         return f"Nome do responsável: {self.GuardianName}"
 
     def __str__(self):
-        return f"Nome: {self.name}, Idade: {self.age}"
+        return f"Nome: {self.name} \nIdade: {self.age} \nEstado Civil: {self.civil_status} \nProfissão: {self.profession}"
         # Data de Nascimento: {self.birthday}, Gênero: {self.gender}, CPF: {self.name}, RG: {self.RG}, Endereço: {self.adress}, Contato: {self.contact}, Data da primeira sessão: {self.FirstSession}, ID: {self.id}"
 
 class Session:
-    def __init__(self, date, hour): #, duration, type, status, content)
+    def __init__(self, date, hour, queixa_principal, historico_familiar, historico_pessoal, aspectos, obs): #, duration, type, status, queixa_principal)
         self.date = date
         self.hour = hour
         # self.duration = duration
         # self.type = type
         # self.status = status
-        # self.content = content
+        self.queixa_principal = queixa_principal
+        self.historico_familiar = historico_familiar
+        self.historico_pessoal = historico_pessoal
+        self.aspectos = aspectos
+        self.obs = obs
 
     def __str__(self):
-        return f"Data: {self.date}, Horário: {self.hour}" #, Conteúdo: {self.content}"
-
+        return f"Data: {self.date}, Horário: {self.hour}\n\nQueixa Principal: {self.queixa_principal} \n\nHistórico Familiar: {self.historico_familiar} \n\nHistórico Pessoal: {self.historico_pessoal} \n\nAspectos emocionais e comportamentais: {self.aspectos} \n\nObservações: {self.obs}"
+ 
 def create():
     name = input("Insira o nome do paciente: ")
     age = int(input("Insira a idade do Paciente: "))
-    paciente = Paciente(name, age)
+    civil_status = input("Estado Cívil do paciente: ")
+    profession = input("Insira a profissão do paciente: ")
+    paciente = Paciente(name, age, civil_status, profession)
     date = input("Insira a data do atendimento: ")
     hour = input("Insira o horário da sessão: ")
-    session = Session(date, hour)
+    queixa_principal = input("Queixa principal: ")
+    historico_familiar = input("Histórico Familiar: ")
+    historico_pessoal = input("Histórico Pessoal: ")
+    aspectos = input("Aspectos emocionais e comportamentais: ")
+    obs = input("Observações: ")
+    session = Session(date, hour, queixa_principal, historico_familiar, historico_pessoal, aspectos, obs)
 
     if age < 18:
         GuardianName = input("Qual o nome do responsável: ")
@@ -86,10 +77,10 @@ def update():
         for i in patient:
             print(i, end="")
     
-    content = input("\nDigite o conteúdo da Alteração: \n")
+    queixa_principal = input("\nDigite o conteúdo da Alteração: \n")
     with open (f"./TestesBrutos/Data/Paciente_{name}", "a", encoding="utf-8") as patient:
-        patient.write(f"\n{str(content)}\n")
-
+        patient.write(f"\n{str(queixa_principal)}\n")
+# 
 def remove():
     name = input("Insira o nome do paciente: ")
     file = f"./TestesBrutos/Data/Paciente_{name}"
@@ -98,7 +89,7 @@ def remove():
     else:
         print("O arquivo não existe.")
 
-def error():
+def invalid():
     print("Opção inválida.")
 
 
@@ -112,7 +103,7 @@ Menu = {
 os.system('cls')
 
 while True:
-    print("----- Gerenciamento de Pacientes -----")
+    print("\n----- Gerenciamento de Pacientes -----")
     print("\n1.Criar novo paciente  " \
         "\n2.Ler o prontuário " \
         "\n3.Atualizar o prontuário " \
@@ -122,4 +113,4 @@ while True:
     option = int(input("Selecione a opção desejada: "))
     if option == 5:
         break
-    Menu.get(option, error)()
+    Menu.get(option, invalid)()
